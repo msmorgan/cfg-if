6c76ffe2749be91f08ac8dbb2f1a5dca3a38b754
Michael Morgan <morgan.michael@me.com>
2020-12-04 12:11:30 -0500

    Reformatting mk1

--- src/lib.rs
+++ src/lib.rs
@@ -33,32 +33,37 @@
 #[macro_export]
 macro_rules! cfg_if {
     // match if/else chains with a final `else`
-    ($(
-        if #[cfg($meta:meta)] { $($tokens:tt)* }
-    ) else * else {
-        $($tokens2:tt)*
-    }) => {
+    (
+        $(
+            if #[cfg( $i_meta:meta )] { $( $i_tokens:tt )* }
+        ) else*
+        else { $( $e_tokens:tt )* }
+    ) => {
         $crate::cfg_if! {
             @__items
             () ;
-            $( ( ($meta) ($($tokens)*) ), )*
-            ( () ($($tokens2)*) ),
+            $(
+                ( ($i_meta) ( $( $i_tokens )* ) ),
+            )*
+            ( () ($($e_tokens)*) ),
         }
     };
 
     // match if/else chains lacking a final `else`
     (
-        if #[cfg($i_met:meta)] { $($i_tokens:tt)* }
+        if #[cfg( $i_meta:meta )] { $( $i_tokens:tt )* }
         $(
-            else if #[cfg($e_met:meta)] { $($e_tokens:tt)* }
+            else if #[cfg( $e_meta:meta )] { $( $e_tokens:tt )* }
         )*
     ) => {
         $crate::cfg_if! {
             @__items
             () ;
-            ( ($i_met) ($($i_tokens)*) ),
-            $( ( ($e_met) ($($e_tokens)*) ), )*
-            ( () () ),
+            ( ( $i_meta ) ( $( $i_tokens )* ) ) ,
+            $(
+                ( ( $e_meta ) ( $( $e_tokens )* ) ) ,
+            )*
+            ( () () ) ,
         }
     };
 
@@ -66,23 +71,32 @@ macro_rules! cfg_if {
     //
     // Collects all the negated cfgs in a list at the beginning and after the
     // semicolon is all the remaining items
-    (@__items ($($not:meta,)*) ; ) => {};
-    (@__items ($($not:meta,)*) ; ( ($($m:meta),*) ($($tokens:tt)*) ), $($rest:tt)*) => {
+    (@__items ( $( $not:meta , )* ) ; ) => {};
+    (
+        @__items ( $( $not:meta , )* ) ;
+        ( ( $( $m:meta ),* ) ( $( $tokens:tt )* ) ) ,
+        $( $rest:tt , )*
+    ) => {
         // Emit all items within one block, applying an appropriate #[cfg]. The
         // #[cfg] will require all `$m` matchers specified and must also negate
         // all previous matchers.
-        #[cfg(all($($m,)* not(any($($not),*))))] $crate::cfg_if! { @__identity $($tokens)* }
+        #[cfg(all( $( $m , )* not(any( $( $not ),* )) ))]
+        $crate::cfg_if! { @__identity $( $tokens )* }
 
         // Recurse to emit all other items in `$rest`, and when we do so add all
         // our `$m` matchers to the list of `$not` matchers as future emissions
         // will have to negate everything we just matched as well.
-        $crate::cfg_if! { @__items ($($not,)* $($m,)*) ; $($rest)* }
+        $crate::cfg_if! {
+            @__items
+            ( $( $not , )* $( $m , )* ) ;
+            $( $rest , )*
+        }
     };
 
     // Internal macro to make __apply work out right for different match types,
     // because of how macros matching/expand stuff.
-    (@__identity $($tokens:tt)*) => {
-        $($tokens)*
+    (@__identity $( $tokens:tt )* ) => {
+        $( $tokens )*
     };
 }
 
