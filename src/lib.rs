1b450ac39b04c89b93d325c8a2d4cbd3f7c766fc
Michael Morgan <morgan.michael@me.com>
2020-12-04 13:11:00 -0500

    yes?

--- src/lib.rs
+++ src/lib.rs
@@ -36,16 +36,15 @@ macro_rules! cfg_if {
     (
         $(
             if #[cfg( $i_meta:meta )] { $( $i_tokens:tt )* }
-        ) else*
+        ) else+
         else { $( $e_tokens:tt )* }
     ) => {
         $crate::cfg_if! {
-            @__items
-            () ;
+            @__items () ;
             $(
-                ( ( $i_meta ) ( $( $i_tokens )* ) ),
-            )*
-            ( () ( $( $e_tokens )* ) ),
+                ( ( $i_meta ) ( $( $i_tokens )* ) ) ,
+            )+
+            ( () ( $( $e_tokens )* ) ) ,
         }
     };
 
@@ -57,8 +56,7 @@ macro_rules! cfg_if {
         )*
     ) => {
         $crate::cfg_if! {
-            @__items
-            () ;
+            @__items () ;
             ( ( $i_meta ) ( $( $i_tokens )* ) ) ,
             $(
                 ( ( $e_meta ) ( $( $e_tokens )* ) ) ,
@@ -71,31 +69,36 @@ macro_rules! cfg_if {
     //
     // Collects all the negated cfgs in a list at the beginning and after the
     // semicolon is all the remaining items
-    (@__items ( $( $no:meta , )* ) ; ) => {};
+    ( @__items ( $( $_:meta , )* ) ; ) => {};
     (
         @__items ( $( $no:meta , )* ) ;
-        ( ( $( $yes:meta ),* ) ( $( $tokens:tt )* ) ) ,
+        ( ( $( $yes:meta )? ) ( $( $tokens:tt )* ) ) ,
         $( $rest:tt , )*
     ) => {
         // Emit all items within one block, applying an appropriate #[cfg]. The
         // #[cfg] will require all `$yes` matchers specified and must also negate
         // all previous matchers.
-        #[cfg(all( $( $yes , )* not(any( $( $no ),* )) ))]
+        #[cfg(all(
+            $( $yes , )?
+            not(any( $( $no ),* ))
+        ))]
         $crate::cfg_if! { @__identity $( $tokens )* }
 
         // Recurse to emit all other items in `$rest`, and when we do so add all
         // our `$yes` matchers to the list of `$no` matchers as future emissions
         // will have to negate everything we just matched as well.
         $crate::cfg_if! {
-            @__items
-            ( $( $no , )* $( $yes , )* ) ;
+            @__items (
+                $( $no , )*
+                $( $yes , )?
+            ) ;
             $( $rest , )*
         }
     };
 
     // Internal macro to make __apply work out right for different match types,
     // because of how macros matching/expand stuff.
-    (@__identity $( $tokens:tt )* ) => {
+    ( @__identity $( $tokens:tt )* ) => {
         $( $tokens )*
     };
 }
